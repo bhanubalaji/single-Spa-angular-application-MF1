84c705714b987e5645be8e9dd21112b92bfa39a2
B BhanuBalaji <bezawada_balaji@nextazy.com>
2024-08-23 18:52:11 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-prject-MF';
+export class AppComponent  {
+
+  title = 'angular-project-MF1';
+
+
+      value:any
+      onButtonClick(){
+        // In Angular application 1
+      const event = new CustomEvent('app1Event', {
+        detail: { message: this.value }
+      });
+      window.dispatchEvent(event);
+      }
+  
 }
