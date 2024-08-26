fbfe9e07ad786b28cc86be1e8e0631141c6a1e80
B BhanuBalaji <bezawada_balaji@nextazy.com>
2024-08-26 16:15:56 +0530

    add bus events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,32 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+
 
   title = 'angular-project-MF1';
+  value: any;
 
-
-      value:any
-      onButtonClick(){
-        // In Angular application 1
-      const event = new CustomEvent('app1Event', {
-        detail: { message: this.value }
-      });
-      window.dispatchEvent(event);
+  ngOnInit(): void {
+    window.addEventListener('message', (event) => {
+      if (event.origin !== 'http://localhost:9000') {
+        return; // Ignore messages from unexpected origins
       }
-  
+    
+      const message = event.data?.message;
+      if(message){
+      console.log('Received message:', message);
+      }
+      // Handle the message
+    });
+  }
+
+  onButtonClick() {
+    // In Angular application 1
+    const event = new CustomEvent('app1Event', {
+      detail: { message: this.value }
+    });
+    window.dispatchEvent(event);
+  }
+
 }
